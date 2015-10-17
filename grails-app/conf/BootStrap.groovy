8cfbf9b06a2edda749b90111b601f72687cd460d
Lucian <lucianilie@yahoo.com>
2015-10-17 22:18:06 +0300

    #58925771204106 Add Spring Security

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,24 @@
+import nonschool.security.Role
+import nonschool.security.User
+import nonschool.security.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+//        create admin user and role
+        adminUserInit()
+
     }
+
     def destroy = {
+    }
+
+    def adminUserInit() {
+        User adminUser = new User("admin", "admin")
+        Role adminRole = new Role("ROLE_ADMIN")
+        adminUser.save(flush: true)
+        adminRole.save(flush: true)
+
+        UserRole.create adminUser, adminRole, true
     }
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,19 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'nonschool.security.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'nonschool.security.UserRole'
+grails.plugin.springsecurity.authority.className = 'nonschool.security.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+        '/'              : ['permitAll'],
+        '/index'         : ['permitAll'],
+        '/index.gsp'     : ['permitAll'],
+        '/assets/**'     : ['permitAll'],
+        '/**/js/**'      : ['permitAll'],
+        '/**/css/**'     : ['permitAll'],
+        '/**/images/**'  : ['permitAll'],
+        '/**/favicon.ico': ['permitAll']
+]
+
