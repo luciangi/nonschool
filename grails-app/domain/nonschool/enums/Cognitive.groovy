ef6c4308bae2797a7a5cb235663556995d29e6b0
nestedloop <adrianpanzariu@gmail.com>
2016-03-06 23:05:36 +0200

    Housekeeping

--- grails-app/domain/nonschool/enums/Cognitive.groovy
+++ grails-app/domain/nonschool/enums/Cognitive.groovy
@@ -1,8 +1,5 @@
 package nonschool.enums
 
-/**
- * Created by nestedloop on 2/27/2016.
- */
 enum Cognitive {
     Remember, Understand, Apply, Analyze, Evaluate, Create
 }
--- grails-app/domain/nonschool/enums/Knowledge.groovy
+++ grails-app/domain/nonschool/enums/Knowledge.groovy
@@ -1,8 +1,5 @@
 package nonschool.enums
 
-/**
- * Created by nestedloop on 2/27/2016.
- */
 enum Knowledge {
     Fact, Concept, Process, Procedure, Principle, Metacognitive
 }
